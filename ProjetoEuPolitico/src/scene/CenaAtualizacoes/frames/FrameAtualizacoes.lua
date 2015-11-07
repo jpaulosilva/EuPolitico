f3c8920e2f9311b4dbf9613e268eeea4d58a6ce5
jpaulosilva <jpaulo_silva@yahoo.com.br>
2015-11-06 23:33:28 -0300

    Navegação menu

--- ProjetoEuPolitico/src/scene/CenaAtualizacoes/frames/FrameAtualizacoes.lua
+++ ProjetoEuPolitico/src/scene/CenaAtualizacoes/frames/FrameAtualizacoes.lua
@@ -20,6 +20,9 @@ function FrameAtualizacoes:inicialize()
   local font_label= Fonte.new({nome='tiresias', tamanho=24, is_negrito = true});
   font_label.cor = Cor.new({r=0,g=0,b=0});
   
+  local font_logo= Fonte.new({nome='tiresias', tamanho=24, is_negrito = true});
+  font_logo.cor = Cor.new({r=255,g=94,b=94});
+  
   
   local label = TLabel.new();
   label:setTexto("Frame Atualizações");
@@ -27,6 +30,24 @@ function FrameAtualizacoes:inicialize()
   label:setPx(10);
   label:setPy(10);
   
+  --*************************Legendas**************************************
+  local labelBtTelaInicial = TLabel.new();
+  labelBtTelaInicial:setTexto("TELA INICIAL");
+  labelBtTelaInicial:setFonte(font_logo);
+
+  local imageBtTelaInicial= TImage.new();
+  imageBtTelaInicial:setSrcArquivoExterno("../media/legenda_vermelho_30.png");
+
+
+  local iconeBtTelaInicial = TIcon.new();
+  iconeBtTelaInicial:setTImage(imageBtTelaInicial);
+  iconeBtTelaInicial:setTLabel(labelBtTelaInicial);
+  iconeBtTelaInicial:setOrientacao(TIcon.TITULO_RIGHT);
+  iconeBtTelaInicial:setPx(1050);
+  iconeBtTelaInicial:setPy(660);
+  
+  
   FrameAtualizacoes:addComponent(label,1);
+  FrameAtualizacoes:addComponent(iconeBtTelaInicial,2);
 	
 end
--- ProjetoEuPolitico/src/scene/CenaBusca/frames/FramePesquisasResultados.lua
+++ ProjetoEuPolitico/src/scene/CenaBusca/frames/FramePesquisasResultados.lua
@@ -20,6 +20,8 @@ function FramePesquisasResultados:inicialize()
   local font_label= Fonte.new({nome='tiresias', tamanho=24, is_negrito = true});
   font_label.cor = Cor.new({r=0,g=0,b=0});
   
+  local font_logo= Fonte.new({nome='tiresias', tamanho=24, is_negrito = true});
+  font_logo.cor = Cor.new({r=255,g=94,b=94});
   
   local label = TLabel.new();
   label:setTexto("Frame Pesquisas e Resultados");
@@ -27,6 +29,23 @@ function FramePesquisasResultados:inicialize()
   label:setPx(10);
   label:setPy(10);
   
+  --*************************Legendas**************************************
+  local labelBtTelaInicial = TLabel.new();
+  labelBtTelaInicial:setTexto("TELA INICIAL");
+  labelBtTelaInicial:setFonte(font_logo);
+
+  local imageBtTelaInicial= TImage.new();
+  imageBtTelaInicial:setSrcArquivoExterno("../media/legenda_vermelho_30.png");
+
+
+  local iconeBtTelaInicial = TIcon.new();
+  iconeBtTelaInicial:setTImage(imageBtTelaInicial);
+  iconeBtTelaInicial:setTLabel(labelBtTelaInicial);
+  iconeBtTelaInicial:setOrientacao(TIcon.TITULO_RIGHT);
+  iconeBtTelaInicial:setPx(1050);
+  iconeBtTelaInicial:setPy(660);
+  
   FramePesquisasResultados:addComponent(label,1);
+  FramePesquisasResultados:addComponent(iconeBtTelaInicial,2);
   
 end
